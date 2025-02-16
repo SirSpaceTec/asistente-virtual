8ca4aeb9353f79236b45ad8ad81851165d81df87
SirSpaceTec <sirspacetech@gmail.com>
2025-02-16 16:05:03 +0100

    model-ai file name to modelAi. Configuring model behavior. Simulate model response when speaking

--- logic/microphone.py
+++ logic/microphone.py
@@ -1,29 +1,37 @@
 import speech_recognition as sr
-
+from logic.modelAi import simular_respuesta_model
 
 def input_microphone():
   count = 0
   r = sr.Recognizer()
-  with sr.Microphone() as source:
-    r.pause_threshold = 2
-    r.adjust_for_ambient_noise(source, duration=1)
-    while True:
-      print(f"Say something, count {count}")
-      try:
-        audio = r.listen(source, timeout=4)
-        text = r.recognize_google(audio, language="es-ES")
-        print(text)
-        count = 0
-        if text.lower() == "apagar asistente":
-          print("Apagando asistente")
-          break
-      except sr.WaitTimeoutError:
-        print("I haven't heard anything")
-      except sr.UnknownValueError:
-        print("I couldn't understand the audio")
-        count += 1
-        if count == 3:
-          break
+  try:
+    with sr.Microphone() as source:
+      r.pause_threshold = 2
+      r.adjust_for_ambient_noise(source, duration=1)
+      while True:
+        print(f"Say something, count {count}")
+        try:
+          audio = r.listen(source, timeout=4)
+          text = r.recognize_google(audio, language="es-ES")
+          print(text)
+          count = 0
+          if text.lower() == "apagar asistente":
+            print("Apagando asistente")
+            break
+          simular_respuesta_model(text)
+          print("Respuesta terminada")
+        except sr.WaitTimeoutError:
+          print("I haven't heard anything")
+        except sr.UnknownValueError:
+          print("I couldn't understand the audio")
+          count += 1
+          if count == 3:
+            break
+  except OSError as e:
+      print(f"Error de micrófono: {e}")
+      print("Intenta reconectar tu micrófono o verifica la configuración del sistema.")
+  except Exception as e:
+      print(f"Ocurrió un error inesperado: {e}")
         
         
         
---
+++ logic/modelAi.py
@@ -0,0 +1,61 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import tiktoken
+import time
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY") 
+
+client = OpenAI(api_key=api_key)
+
+total_tokens_global = 0
+
+def count_tokens(messages, model="gpt-4o-mini"):
+  encoder = tiktoken.encoding_for_model(model)
+  return sum(len(encoder.encode(message["content"])) for message in messages)
+
+def generate_short_response(question):
+  global total_tokens_global
+  messages=[
+        {"role": "system", "content": "Responde breve y preciso. Actúa como un amigo vacilón y gracioso, que siempre responde con humor, juegos de palabras y un toque de sarcasmo amable."},
+        {"role": "user", "content": question}
+        ]
+  
+  stream = client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=messages,
+      stream=True,
+      max_tokens=100,
+      temperature=0.8,
+      top_p=0.9
+  )
+  tokens_sent = count_tokens(messages)
+  print(f"Tokens enviados: {tokens_sent}")
+  
+  response_text = ""
+  for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+      print(chunk.choices[0].delta.content, end="", flush=True)
+      response_text += chunk.choices[0].delta.content
+      
+  tokens_received = len(tiktoken.encoding_for_model("gpt-4o-mini").encode(response_text))
+  total_tokens_used = tokens_sent + tokens_received
+    
+  total_tokens_global += total_tokens_used
+  print(f"\nTokens utilizados en esta consulta: {total_tokens_used}")
+  print(f"Tokens totales acumulados: {total_tokens_global}") 
+  
+  
+  
+def simular_respuesta_model(question = ""):
+  
+  print("Procesando respuesta...")
+  for i in range(3,0,-1):
+    print(f"{i}...", end="", flush=True)
+    time.sleep(1)
+  print("\n")
+  respuesta_simulada = "Esta es la respuesta del GPT simulado"
+  return respuesta_simulada
+
+generate_short_response("Explicame la balanza de pagos")
